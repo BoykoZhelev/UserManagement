349d9a382475b24b98edb6f38aa3f3ec2b7e3097
BoykoZhelev <boyko.zhelev@gmail.com>
2019-05-03 14:10:40 +0300

    Persons Controller change

--- src/main/java/com/management/controller/PersonController.java
+++ src/main/java/com/management/controller/PersonController.java
@@ -13,7 +13,7 @@ import java.util.List;
 /*
  * @author Boyko Zhelev
  */
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RestController
 @RequestMapping("/")
 public class PersonController {
