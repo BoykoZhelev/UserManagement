6b42341c26dee0b3719a0eb6ddb5518664935893
Boyko Zhelev <boyko.zhelev@gmail.com>
2019-05-08 21:35:30 +0300

    To hateoas

--- src/main/java/com/management/controller/PersonController.java
+++ src/main/java/com/management/controller/PersonController.java
@@ -3,12 +3,21 @@ package com.management.controller;
 import com.management.model.Person;
 import com.management.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.ws.rs.NotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 /*
  * @author Boyko Zhelev
@@ -21,43 +30,50 @@ public class PersonController {
     @Autowired
     PersonService personService;
 
-    @GetMapping("/all")
-    public ResponseEntity<List<Person>> findAll(){
+    @GetMapping("/")
+    public Resources<Resource<Person>> findAll(){
 
-        List<Person> people = personService.findAll();
-        if(people==null){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(people,HttpStatus.OK);
+        List<Resource<Person>> people = personService.findAll().stream().map(person ->
+                new Resource<>(person,
+                        linkTo(methodOn(PersonController.class).getPerson(person.getId())).withSelfRel(),
+                        linkTo(methodOn(PersonController.class).findAll()).withRel("/"))).collect(Collectors.toList());
+
+        return new Resources<>(people,linkTo(methodOn(PersonController.class).findAll()).withSelfRel());
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Person> create(@Valid @RequestBody Person person){
-
-        Person created = personService.save(person);
-        return new ResponseEntity<>(created,HttpStatus.OK);
+    @PostMapping("/")
+    public ResponseEntity<Resource<Person>> create(@Valid @RequestBody Person person) throws URISyntaxException {
+        Person saved = personService.save(person);
+        Resource<Person> resource = new Resource<>(saved,linkTo(methodOn(PersonController.class).getPerson(saved.getId())).withSelfRel(),
+                linkTo(methodOn(PersonController.class).findAll()).withRel("/"));
+        return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Person> update(@PathVariable Long id ,@Valid @RequestBody Person person){
+    @PutMapping("/{id}")
+    public ResponseEntity<Resource<Person>> update(@PathVariable Long id ,@Valid @RequestBody Person person) throws URISyntaxException {
 
-        boolean ok =personService.update(id,person);
-        return new ResponseEntity<>(ok ?HttpStatus.OK: HttpStatus.NOT_FOUND);
+        personService.update(id,person);
+        Person updated = personService.findById(id);
+            Resource<Person> resource = new Resource<>(updated,linkTo(methodOn(PersonController.class).getPerson(updated.getId())).withSelfRel(),
+                    linkTo(methodOn(PersonController.class).findAll()).withRel("/"));
+            return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Person> delete(@PathVariable Long id){
-        boolean ok =personService.deleteById(id);
-        return new ResponseEntity<>(ok ?HttpStatus.OK: HttpStatus.NOT_FOUND);
+        personService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/find/{id}")
-    public ResponseEntity<Person> getPerson(@PathVariable Long id){
-        if(personService.findById(id)!=null) {
-             return new ResponseEntity<>(personService.findById(id),HttpStatus.OK);
+    @GetMapping("/{id}")
+    public Resource<Person> getPerson(@PathVariable Long id){
+        Person person = personService.findById(id);
+        if(person !=null) {
+             return new Resource<>(person,linkTo(methodOn(PersonController.class).getPerson(person.getId())).withSelfRel(),
+                     linkTo(methodOn(PersonController.class).findAll()).withRel("/"));
         }
         else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            throw new NotFoundException("Not found");
         }
     }
 }
