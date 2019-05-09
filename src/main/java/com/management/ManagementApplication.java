07970dcbb10287da69529bec9e8360b38440f2b3
Boyko Zhelev <boyko.zhelev@gmail.com>
2019-05-09 17:18:18 +0300

    WebFilter for http headers

--- src/main/java/com/management/ManagementApplication.java
+++ src/main/java/com/management/ManagementApplication.java
@@ -2,6 +2,7 @@ package com.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@ServletComponentScan
 @SpringBootApplication
 public class ManagementApplication {
 
---
+++ src/main/java/com/management/filters/CorsFilter.java
@@ -0,0 +1,26 @@
+package com.management.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse)response;
+        httpServletResponse.setHeader("Content-Type","application/json");
+        httpServletResponse.setHeader("Accept","json");
+        httpServletResponse.setHeader("Momo","gagaga");
+
+        chain.doFilter(request,httpServletResponse);
+    }
+}
